5fafa93bb42805461797b169135230d2441ad0d5
it0088 <itrf0088@gmail.com>
2023-03-02 00:04:38 +0500

    The project icons, fonts(Montserrat,Poppins),strings,textAppearance are configured

--- app/src/main/java/com/neophron/onlineshop/OnlineShopActivity.kt
+++ app/src/main/java/com/neophron/onlineshop/OnlineShopActivity.kt
@@ -2,14 +2,11 @@ package com.neophron.onlineshop
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.neophron.mylibrary.ktx.getColor
 
 class OnlineShopActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        window.decorView.setBackgroundColor(
-            theme.getColor(com.google.android.material.R.attr.backgroundColor)
-        )
+        window.setBackgroundDrawableResource(R.drawable.day_night)
     }
 }
